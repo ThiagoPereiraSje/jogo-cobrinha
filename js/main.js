a91ca46c7cc7dd4b1810a81016529c22b82bfee4
Thiago Pereira <thiagopereiramail@gmail.com>
2022-03-20 20:18:00 -0300

    Cria o game loop

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 const banner = document.getElementById('banner');
+let timer = undefined;
 
 function start() {
   banner.style.display = 'none';
+
+  if (!timer) {
+    // Game Loop a cada 60ms
+    timer = setInterval(gameLoop, 60);
+  }
+}
+
+function gameLoop() {
+  console.log("Game Loop!");
 }
