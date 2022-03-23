f6037bc1bc6423fdac349e482c17d16edcc7d78c
Thiago Pereira <thiagopereiramail@gmail.com>
2022-03-23 12:29:39 -0300

    Colocando o rosto do primo no jogo

--- js/entities/Food.js
+++ js/entities/Food.js
@@ -1,14 +1,21 @@
 const FOOD_SIZE = 10;
+const PRIMO_SIZE = 30;
+const primo = document.getElementById("primo");
 
 const Food = {
   x: 0,
   y: 0,
   w: FOOD_SIZE,
   h: FOOD_SIZE,
+  super: false,
 
   render: function (ctx) {
-    ctx.fillStyle = "green";
-    ctx.fillRect(this.x, this.y, this.w, this.h);
+    if (this.super) {
+      ctx.drawImage(primo, this.x, this.y, this.w, this.h);
+    } else {
+      ctx.fillStyle = "green";
+      ctx.fillRect(this.x, this.y, this.w, this.h);
+    }
   },
 
   restart: function () {
@@ -17,5 +24,14 @@ const Food = {
 
     this.x = getRandomInt(0, 79) * 10;
     this.y = getRandomInt(0, 49) * 10;
+    this.super = getRandomInt(0, 100) < 70;
+
+    if (this.super) {
+      this.w = PRIMO_SIZE;
+      this.h = PRIMO_SIZE;
+    } else {
+      this.w = FOOD_SIZE;
+      this.h = FOOD_SIZE;
+    }
   },
 };
